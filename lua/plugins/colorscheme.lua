596da15329becf06e21126f34a1ef9c80031f9f7
Pedro <pedro_hga@proton.me>
2024-02-06 15:05:39 -0300

    added emmet-ls and edit colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -32,9 +32,17 @@ return {
     dim_inactive = true,
   },
   {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nordic").load()
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "nordic",
     },
   },
 }
