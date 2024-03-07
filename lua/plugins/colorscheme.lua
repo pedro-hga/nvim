34f13c80ac2a1619de829d8d166ad3eb5e882c14
Pedro <pedro_hga@proton.me>
2024-03-07 11:58:11 -0300

    minor adjustments

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,38 +4,38 @@ return {
     lazy = false,
     priority = 1000,
   },
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --
-  --   priority = 1000,
-  --   config = true,
-  --   opts = {
-  --     terminal_colors = true, -- add neovim terminal colors
-  --     undercurl = true,
-  --     underline = true,
-  --     bold = true,
-  --     italic = {
-  --       strings = true,
-  --       emphasis = true,
-  --       comments = true,
-  --       operators = false,
-  --       folds = true,
-  --     },
-  --     strikethrough = true,
-  --     invert_selection = false,
-  --     invert_signs = false,
-  --     invert_tabline = false,
-  --     invert_intend_guides = false,
-  --     inverse = true, -- invert background for search, diffs, statuslines and errors
-  --     contrast = "hard",
-  --     palette_overrides = {
-  --       -- dark0_hard = "#16181b",
-  --       dark0_hard = "#121212",
-  --     },
-  --   },
-  --   overrides = {},
-  --   dim_inactive = true,
-  -- },
+  {
+    "ellisonleao/gruvbox.nvim",
+
+    priority = 1000,
+    config = true,
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "hard",
+      palette_overrides = {
+        -- dark0_hard = "#16181b",
+        -- dark0_hard = "#121212",
+      },
+    },
+    overrides = {},
+    dim_inactive = true,
+  },
   -- nordic
   -- {
   --   "AlexvZyl/nordic.nvim",
@@ -47,13 +47,13 @@ return {
   -- },
 
   -- nord
-  -- {
-  --   "gbprod/nord.nvim",
-  --   priority = 1000,
-  --   config = function()
-  --     require("nord").setup({})
-  --   end,
-  -- },
+  {
+    "gbprod/nord.nvim",
+    priority = 1000,
+    config = function()
+      require("nord").setup({})
+    end,
+  },
 
   -- --nord2
   -- {
@@ -66,19 +66,19 @@ return {
   --     require("kanagawa").setup({})
   --   end,
   -- },
-  {
-    "sainnhe/everforest-nvim",
-  },
-  {
-    "Yazeed1s/oh-lucy.nvim",
-  },
+  -- {
+  --   "sainnhe/everforest-nvim",
+  -- },
+  -- {
+  --   "Yazeed1s/oh-lucy.nvim",
+  -- },
   {
     "rmehri01/onenord.nvim",
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "miasma",
+      colorscheme = "nord",
     },
   },
 }
