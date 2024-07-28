0b49ec98d919f8568d801c0cc3c04d99f28e1024
Pedro <pedro_hga@proton.me>
2024-07-28 17:08:29 -0300

    changed colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,46 +1,46 @@
 return {
-  {
-    "xero/miasma.nvim",
-    lazy = false,
-    priority = 1000,
-  },
+  -- {
+  --   "xero/miasma.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
   -- {
   --   "fcancelinha/northern.nvim",
   --   branch = "master",
   --   priority = 1000,
   -- },
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --
-  --   priority = 1000,
-  --   config = true,
-  --   opts = {
-  --     terminal_colors = true, -- add neovim terminal colors
-  --     undercurl = true,
-  --     underline = true,
-  --     bold = true,
-  --     italic = {
-  --       strings = true,
-  --       emphasis = true,
-  --       comments = true,
-  --       operators = false,
-  --       folds = true,
-  --     },
-  --     strikethrough = true,
-  --     invert_selection = false,
-  --     invert_signs = false,
-  --     invert_tabline = false,
-  --     invert_intend_guides = false,
-  --     inverse = true, -- invert background for search, diffs, statuslines and errors
-  --     contrast = "hard",
-  --     palette_overrides = {
-  --       -- dark0_hard = "#16181b",
-  --       dark0_hard = "#121212",
-  --     },
-  --   },
-  --   overrides = {},
-  --   dim_inactive = true,
-  -- },
+  {
+    "ellisonleao/gruvbox.nvim",
+
+    priority = 1000,
+    config = true,
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "hard",
+      palette_overrides = {
+        -- dark0_hard = "#16181b",
+        dark0_hard = "#22262F",
+      },
+    },
+    overrides = {},
+    dim_inactive = true,
+  },
   -- nordic({
   --   "AlexvZyl/nordic.nvim",
   --   lazy = false,
@@ -62,9 +62,18 @@ return {
     "EdenEast/nightfox.nvim",
   },
   {
+    "Yazeed1s/minimal.nvim",
+  },
+  {
+    "Yazeed1s/oh-lucy.nvim",
+  },
+  {
+    "kaiuri/nvim-juliana",
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
+      colorscheme = "gruvbox",
     },
   },
 }
