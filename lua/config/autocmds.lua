7bea40b73a21276aebf7041e32ebb464db1533f3
Pedro <pedro_hga@proton.me>
2025-10-09 14:14:43 -0300

    sync

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local augroup = vim.api.nvim_create_augroup("mygroup", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  pattern = "*",
+  callback = function()
+    -- Remove 'o' and 'O' from formatoptions to prevent comment continuation
+    vim.opt_local.formatoptions:remove("o")
+  end,
+  desc = "Don't continue comments with o and O",
+})
